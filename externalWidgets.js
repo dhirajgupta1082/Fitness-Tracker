d00c2ab5904fc9ee0f1a5e1d22e88f5edd56c824
dhirajgupta1082 <dhirajgupta1082@gmail.com>
2024-08-04 14:11:53 +0530

    Add files via upload

---
+++ externalWidgets.js
@@ -0,0 +1,6 @@
+export { AppEvents as com_mendix_widget_native_appevents_AppEvents } from "./widgets/com/mendix/widget/native/appevents/AppEvents";
+export { Carousel as com_mendix_widget_native_carousel_Carousel } from "./widgets/com/mendix/widget/native/carousel/Carousel";
+export { FloatingActionButton as com_mendix_widget_native_floatingactionbutton_FloatingActionButton } from "./widgets/com/mendix/widget/native/floatingactionbutton/FloatingActionButton";
+export { BottomSheet as com_mendix_widget_native_bottomsheet_BottomSheet } from "./widgets/com/mendix/widget/native/bottomsheet/BottomSheet";
+export { PopupMenu as com_mendix_widget_native_popupmenu_PopupMenu } from "./widgets/com/mendix/widget/native/popupmenu/PopupMenu";
+export { Runmap as mendix_runmap_Runmap } from "./widgets/mendix/runmap/Runmap";
---
+++ react-native.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  reactNativePath: 'C:/Program Files/Mendix/8.10.2.394/modeler/tools/node/node_modules/react-native'
+};
